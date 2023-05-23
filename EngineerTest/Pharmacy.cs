302928f8df95b8beb38de51e6a816b896ce62a55
cinnamonscript <94289131+cinnamonscript@users.noreply.github.com>
2023-05-24 00:51:04 +1000

    Refactoring UpdateBenefitValue
    Refactored UpdateBenefitValue to reflect drugs that increase in benefit, and decrease in benefit.
    Update for all drugs as per instructions.

--- EngineerTest/Pharmacy.cs
+++ EngineerTest/Pharmacy.cs
@@ -12,8 +12,8 @@ public class Pharmacy : IPharmacy
     // The Benefit of an item is never more than 50.
     // The Benefit of an item is never negative.
     // Once expiration date, Benefit degrades x2.
-    // "Herbal Tea" increases in Benefit, 2x faster after expiration date.
     // "Magic Pill" never expires nor decreases in Benefit.
+    // "Herbal Tea" increases in Benefit, 2x faster after expiration date.
     // "Fervex" increases in Benefit, x2 when <11d, x3 when <6d, 0 after exp.
     // "Dafalgan" degrades in Benefit twice as fast as normal drugs.
 
@@ -21,74 +21,74 @@ public class Pharmacy : IPharmacy
     {
         for (var i = 0; i < _drugs.Length; i++)
         {
-            // Benefit if not "Herbal Tea", "Fervex", "Magic Pill", "Dafalgan"
+            // Benefit of Benefit-Decreasing Drugs = exclude "Herbal Tea", "Fervex", "Magic Pill"
             if (
                 _drugs[i].Name != "Herbal Tea" &&
                 _drugs[i].Name != "Fervex" &&
                 _drugs[i].Name != "Magic Pill" &&
-                _drugs[i].Name != "Dafalgan"
+                _drugs[i].Benefit > 0 // Benefit does not go negative
             )
             {
-                // If Benefit does not go negative
-                if (_drugs[i].Benefit > 0)
+                // Dafalgan Decreases Benefit by 2
+                if (_drugs[i].Name == "Dafalgan" && _drugs[i].Benefit > 1) // Benefit does not go negative
                 {
-                // Benefit decreases by 1
-                _drugs[i].Benefit = _drugs[i].Benefit - 1;
+                    _drugs[i].Benefit -= 2;
+                }
+
+                // All other Drugs Decrease Benefit by 1, also assume if Dafalgan Benefit = 1 that it will degrade to 0
+                else
+                {
+                    _drugs[i].Benefit--;
                 }
             }
 
-            // Benefit if "Herbal Tea" or "Fervex"
+            // Benefit of Benefit-Increasing Drugs = exclude "Magic Pill", "Dafalgan"
             else
             {
                 // Less than 50
-                if (_drugs[i].Benefit < 50)
+                if (_drugs[i].Benefit < 50 &&
+                _drugs[i].Name != "Magic Pill" &&
+                _drugs[i].Name != "Dafalgan")
                 {
                     // Benefit +1
-                    _drugs[i].Benefit = _drugs[i].Benefit + 1;
-                    
-                    // "Fervex"
+                    _drugs[i].Benefit++;
+
+                    // "Fervex" increases in Benefit, x2 when <11d, x3 when <6d.
                     if (_drugs[i].Name == "Fervex")
                     {
-                        if (_drugs[i].ExpiresIn < 11 && _drugs[i].ExpiresIn > 5)
+                        if (_drugs[i].ExpiresIn < 11)
                         {
-                        _drugs[i].Benefit = _drugs[i].Benefit + 1;
+                            _drugs[i].Benefit++;
                         }
 
                         if (_drugs[i].ExpiresIn < 6)
                         {
-                        _drugs[i].Benefit = _drugs[i].Benefit + 1;
+                            _drugs[i].Benefit++;
                         }
 
-                        if (_drugs[i].ExpiresIn < 0)
-                        {
-                        _drugs[i].Benefit == 0;
-                        }
                     }
 
-                    // "Herbal Tea"
-                    if (_drugs[i].Name == "Herbal Tea")
+                    // "Herbal Tea" increases in Benefit, 2x faster after expiration date.
+                    if (_drugs[i].Name == "Herbal Tea" && _drugs[i].Benefit < 49)
                     {
-                        if (_drugs[i].ExpiresIn < 0)
+                        if (_drugs[i].ExpiresIn <= 0)
                         {
-                        _drugs[i].Benefit = _drugs[i].Benefit + 2;
-                        }
-                    }
-
-                    // "Dafalgan"
-                    if (_drugs[i].Name == "Dafalgan")
-                    {
-                        if (_drugs[i].Benefit > 0)
-                        {
-                        _drugs[i].Benefit = _drugs[i].Benefit - 2;
+                            _drugs[i].Benefit++;
                         }
                     }
                 }
+
+                // "Fervex" Benefit = 0 after exp.
+                if (_drugs[i].Name == "Fervex" && _drugs[i].ExpiresIn < 1)
+                {
+                    _drugs[i].Benefit = 0;
+                }
             }
 
-            // Expiring if not "Magic Pill"
+            // All drugs expire bar "Magic Pill"
             if (_drugs[i].Name != "Magic Pill")
             {
-                _drugs[i].ExpiresIn = _drugs[i].ExpiresIn - 1;
+                _drugs[i].ExpiresIn--;
             }
         }
         return _drugs;
--- EngineerTest/Program.cs
+++ EngineerTest/Program.cs
@@ -9,21 +9,22 @@ try
         new Drug("Doliprane", 20, 30),
         new Drug("Herbal Tea", 10, 5),
         new Drug("Fervex", 12, 35),
-        new Drug("Magic Pill", 15, 40)
+        new Drug("Magic Pill", 15, 40),
         new Drug("Dafalgan", 25, 45)
     };
     IPharmacy pharmacy = new Pharmacy(drugs);
 
     var log = new List<Drug[]?>();
 
-    for (var elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
+    for (var elapsedDays = 0; elapsedDays < 30; elapsedDays++)
+    {
         log.Add(JsonSerializer.Deserialize<Drug[]>(JsonSerializer.Serialize(pharmacy.UpdateBenefitValue())));
     }
 
     File.WriteAllText(
         "output.json",
-        JsonSerializer.Serialize(new { Result= log }, options: new JsonSerializerOptions { WriteIndented = true})
-    );    
+        JsonSerializer.Serialize(new { Result = log }, options: new JsonSerializerOptions { WriteIndented = true })
+    );
     Console.WriteLine("Success");
 
 }
